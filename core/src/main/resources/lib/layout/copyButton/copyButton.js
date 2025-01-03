25e1380ff94ca942ed7dd54ef30f559b75c6c4f0
Zhun-Wei Liu <asd505254@gmail.com>
2025-01-03 15:49:03 +0800

    Make copyButton disabled via Http, and allow with Https

--- core/src/main/resources/lib/layout/copyButton/copyButton.js
+++ core/src/main/resources/lib/layout/copyButton/copyButton.js
@@ -3,36 +3,46 @@ Behaviour.specify(
   "copyButton",
   0,
   function (copyButton) {
-    copyButton.addEventListener("click", () => {
-      if (isSecureContext) {
-        var text = copyButton.getAttribute("text");
-        if (copyButton.hasAttribute("ref")) {
-          var ref = copyButton.getAttribute("ref");
-          var target = document.getElementById(ref);
-          if (target) {
-            text = target.innerText;
-          }
-        }
+    // Check if the connection is secure by evaluating the protocol
+    const isHttps = window.location.protocol === "https:";
 
-        // Copy the text to the clipboard
-        navigator.clipboard
-          .writeText(text)
-          .then(() => {
-            // Show the completion message
-            hoverNotification(copyButton.getAttribute("message"), copyButton);
-          })
-          .catch(() => {
-            hoverNotification(
-              "Could not get permission to write to clipboard",
-              copyButton,
-            );
-          });
-      } else {
-        hoverNotification(
-          "Copy is only supported with a secure (HTTPS) connection",
-          copyButton,
-        );
+    if (!isHttps) {
+      copyButton.disabled = true;
+      copyButton.title = "Copy doesn't work in insecure (HTTP) contexts";
+      return;
+    }
+
+
+    // Enable the button
+    copyButton.disabled = false;
+    copyButton.title = "Click to copy the text";
+
+    // Add click event listener
+    copyButton.addEventListener("click", () => {
+      let text = copyButton.getAttribute("text");
+
+      // If the "ref" attribute is present, fetch the target element's text
+      if (copyButton.hasAttribute("ref")) {
+        const ref = copyButton.getAttribute("ref");
+        const target = document.getElementById(ref);
+        if (target) {
+          text = target.innerText;
+        }
       }
+
+      // Copy the text to the clipboard
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          hoverNotification(copyButton.getAttribute("message") || "Copied!", copyButton);
+        })
+        .catch((err) => {
+          console.error("Failed to copy text:", err);
+          hoverNotification(
+            "Could not get permission to write to clipboard",
+            copyButton,
+          );
+        });
     });
-  },
+  }
 );
